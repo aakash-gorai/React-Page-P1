b25126a719de45fe5aaf43087ce13b0bb7c32875
Aakash <aakashkumargorai@gmail.com>
2023-06-25 17:16:45 +0530

    update: import source changed relative path

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Navbar from "C:/Users/Aakash/Desktop/react-page/components/Navbar";
-import MainContent from "C:/Users/Aakash/Desktop/react-page/components/MainContent";
+import { useState } from "react";
+import "./App.css";
+import Navbar from "../components/Navbar";
+import MainContent from "../components/MainContent";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Navbar />
       <MainContent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
